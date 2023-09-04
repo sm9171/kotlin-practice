4dc09228d5e8324273504a2041b7826ad30f451f
Sangmin <sm9171@nate.com>
2023-09-04 22:58:14 +0900

    feat: Kotlin Extension 생성

--- src/test/kotlin/com/example/kotlin/integration/IntegrationTests.kt
+++ src/test/kotlin/com/example/kotlin/integration/IntegrationTests.kt
@@ -23,7 +23,7 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
         println(">> Assert blog page title, content and status code")
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        assertThat(entity.body).contains("blog")
     }
 
     @Test
