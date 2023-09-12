bef281cf403a174bb1836653955b921c2be574fe
Sangmin <sm9171@nate.com>
2023-09-12 15:11:26 +0900

    style: tab -> space로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,129 +1,129 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.3"
-	id("io.spring.dependency-management") version "1.1.3"
-	id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
-	kotlin("plugin.jpa") version "1.8.22"
-	kotlin("plugin.allopen") version "1.8.22"
-	jacoco
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+    kotlin("plugin.jpa") version "1.8.22"
+    kotlin("plugin.allopen") version "1.8.22"
+    jacoco
 }
 
 allOpen {
-	annotation("javax.persistence.Entity")
-	annotation("javax.persistence.Embeddable")
-	annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("com.h2database:h2")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(module = "junit")
-		exclude(module = "mockito-core")
-	}
-	testImplementation("org.junit.jupiter:junit-jupiter-api")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-	testImplementation("com.ninja-squad:springmockk:3.0.1")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	finalizedBy(tasks.jacocoTestReport)
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
 
-	dependsOn(tasks.test)
-	reports {
-		html.required = true
-		html.outputLocation = file("$buildDir/reports/myReport.html")
-		csv.required = true
-		xml.required = true
-	}
+    dependsOn(tasks.test)
+    reports {
+        html.required = true
+        html.outputLocation = file("$buildDir/reports/myReport.html")
+        csv.required = true
+        xml.required = true
+    }
 
-	var excludes = mutableListOf<String>()
-	excludes.add("com/example/kotlin/BlogConfiguration.kt")
-	excludes.add("com/example/kotlin/Extensions.kt")
-	excludes.add("com/example/kotlin/KotlinApplication.kt")
+    var excludes = mutableListOf<String>()
+    excludes.add("com/example/kotlin/BlogConfiguration.kt")
+    excludes.add("com/example/kotlin/Extensions.kt")
+    excludes.add("com/example/kotlin/KotlinApplication.kt")
 
-	classDirectories.setFrom(
-		sourceSets.main.get().output.asFileTree.matching {
-			exclude(excludes)
-		}
-	)
+    classDirectories.setFrom(
+        sourceSets.main.get().output.asFileTree.matching {
+            exclude(excludes)
+        }
+    )
 
-	finalizedBy(tasks.jacocoTestCoverageVerification)
+    finalizedBy(tasks.jacocoTestCoverageVerification)
 }
 
 tasks.jacocoTestCoverageVerification {
 
-	var Qdomains = mutableListOf<String>()
+    var Qdomains = mutableListOf<String>()
 
-	for (qPattern in 'A' .. 'Z') {
-		Qdomains.add("*.Q${qPattern}*")
-	}
+    for (qPattern in 'A'..'Z') {
+        Qdomains.add("*.Q${qPattern}*")
+    }
 
-	violationRules {
-		rule {
-			enabled = true
-			element = "CLASS"
+    violationRules {
+        rule {
+            enabled = true
+            element = "CLASS"
 
-			limit {
-				counter = "BRANCH"
-				value = "COVEREDRATIO"
-				minimum = "0.00".toBigDecimal()
-			}
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "0.00".toBigDecimal()
+            }
 
-			limit {
-				counter = "LINE"
-				value = "TOTALCOUNT"
-				maximum = "200".toBigDecimal()
-			}
+            limit {
+                counter = "LINE"
+                value = "TOTALCOUNT"
+                maximum = "200".toBigDecimal()
+            }
 
-			excludes = Qdomains
-		}
-	}
+            excludes = Qdomains
+        }
+    }
 
-	var excludes = mutableListOf<String>()
-	excludes.add("com/example/kotlin/BlogConfiguration.kt")
-	excludes.add("com/example/kotlin/Extensions.kt")
-	excludes.add("com/example/kotlin/KotlinApplication.kt")
+    var excludes = mutableListOf<String>()
+    excludes.add("com/example/kotlin/BlogConfiguration.kt")
+    excludes.add("com/example/kotlin/Extensions.kt")
+    excludes.add("com/example/kotlin/KotlinApplication.kt")
 
-	classDirectories.setFrom(
-		sourceSets.main.get().output.asFileTree.matching {
-			exclude(excludes)
-		}
-	)
+    classDirectories.setFrom(
+        sourceSets.main.get().output.asFileTree.matching {
+            exclude(excludes)
+        }
+    )
 }
